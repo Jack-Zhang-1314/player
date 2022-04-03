10ba945b5b50858435fc657edd3d8c7cfdada6b7
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-04-03 15:23:41 +0800

    fix workflows

--- docs/assets/main.366e7a54.js
+++
@@ -1,2 +0,0 @@
-import{d as n,o as r,c as s,u as p}from"./index.8c666848.js";const i=n({setup(u){const t=p(),o=()=>{t.push("/player")};return(a,e)=>(r(),s("button",{onClick:e[0]||(e[0]=c=>o())},"player"))}});export{i as default};
-//# sourceMappingURL=main.366e7a54.js.map
