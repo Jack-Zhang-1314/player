429fc60dcc33b794ca9ff231a3ff17e547cc6f75
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-04-20 16:59:09 +0800

    flip

--- src/components/flip/index.ts
+++ src/components/flip/index.ts
@@ -1,5 +1,5 @@
 import flip from "./flip-child.vue"
 import children from "./main-child.vue"
 import ImageURI from "./ImageURI.vue"
-import Container from "./Container.vue"
+import Container from "./container.vue"
 export { flip, children, ImageURI, Container }
